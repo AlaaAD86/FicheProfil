58d5f68ee3b5c766eeb89ab689c01670e1154df1
AlaaAD86 <alaa.ahmad@greta-sud-aquitaine.academy>
2019-10-12 10:51:12 +0200

    modif-login+flipCSS

---
+++ profil.js
@@ -0,0 +1,108 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+
+Chart.defaults.global.title,
+Chart.defaults.global.tooltips,
+Chart.defaults.global.legend.fontFamily = 'Permanent Marker, cursive'
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Comprendre', 'Dupliquer', 'Analyser', 'Evaluer', 'Concevoir', 'Inventer'],
+        datasets: [{
+            label: 'Maquetter une application',
+            data: [70, 80, 75, 50, 90, 80],
+            backgroundColor: [
+                'rgba(21,60,73, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },{
+            label: 'Réaliser une interface utilisateur web statique et adaptable',
+            data: [78, 36, 13, 29, 80, 88],
+            backgroundColor: [
+                'rgba(217,171,119,0.2)'            ],
+            borderColor: [
+                'rgba(0, 99, 132, 1)',
+                'rgba(0, 162, 235, 1)',
+                'rgba(0, 206, 86, 1)',
+                'rgba(0, 192, 192, 1)',
+                'rgba(0, 102, 255, 1)',
+                'rgba(0, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },{
+            label: 'Développer une interface utilisateur web dynamique',
+            data: [10, 40, 66, 98, 57, 13],
+            backgroundColor: [
+                'rgba(97,20,28,0.2)'            ],
+            borderColor: [
+                'rgba(0, 99, 132, 1)',
+                'rgba(0, 162, 235, 1)',
+                'rgba(0, 206, 86, 1)',
+                'rgba(0, 192, 192, 1)',
+                'rgba(0, 102, 255, 1)',
+                'rgba(0, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },{
+            label: 'Réaliser une interface utilisateur avec une solution de gestion de contenu ou e-commerce',
+            data: [30, 15, 10, 28, 88, 39],
+            backgroundColor: [
+                'rgba(255,91,91,0.2)'            ],
+            borderColor: [
+                'rgba(0, 0, 132, 1)',
+                'rgba(0, 02, 235, 1)',
+                'rgba(0, 06, 86, 1)',
+                'rgba(0, 02, 210, 1)',
+                'rgba(0, 02, 255, 1)',
+                'rgba(0, 09, 64, 1)'
+            ],
+            borderWidth: 1
+        }
+    ]
+    },
+    options: {
+        legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+                fontColor: "#fff",
+                fontSize: 14,
+                fontFamily: 'Nunito, sans-serif'
+            }
+        },
+        title: {
+            display: false,
+            text: 'Custom Chart Title'
+        },
+        scales: {   
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    zeroLineColor: 'rgba(255, 0, 0, 1)',
+                    display: true,
+                    fontFamily: 'Nunito, sans-serif',
+                    fontColor: "rgba(255, 255, 255, 0.5)",
+                    max: 100
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    display: true,
+                    fontFamily: 'Nunito, sans-serif',
+                    fontColor: "rgba(255, 255, 255)",
+                    max: 100
+                }
+            }]
+            
+
+        }
+    }
+});
